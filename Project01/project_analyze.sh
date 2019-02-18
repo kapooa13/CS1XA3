253390c402894454c22ed81e65ca1365eff96e95
Ankit Kapoor <kapooa13@mcmaster.ca>
2019-02-18 04:36:12 +0000

    Implemented Delete temp files feature in project_analyze.sh

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -1,45 +1,73 @@
 #!/bin/bash
 
 #PS3:The value of this parameter is used as a prompt for the select command used below
-PS3="Enter the number to call corresponding function: "
+PS3=$'\n'">>> Enter the number to call corresponding function: "
 
 cd ..
-if [ -e todo.log ]
-then
-    rm todo.log
-    touch todo.log
-else
-    touch todo.log
-fi
+
 
 function todo {
-    grep -r --exclude=project_analyze.sh "#TODO" | while read -r line
+    rm -f todo.log
+    touch todo.log
+    grep -r --exclude=project_analyze.sh --exclude=README.md "#TODO" | while read -r line
     do
     echo "$line" >> todo.log
 done
 }
-echo "--------"
-echo "You are currently running project_analyze.sh"
-echo "--------"
-select answer in TODO_log Exit
+
+function deltempfile {
+    git ls-files --exclude-standard --others | grep ".tmp" | while read -r line
+    do
+    rm "$line"
+done
+}
+
+echo "<<< --------"
+echo "<<< You are currently running project_analyze.sh"
+echo "<<< --------"
+
+select answer in TODO_log Delete_temp_files Exit
 do
     case $answer in
         TODO_log)
-            echo "--------"
-            echo "Executing TODO log"
-            echo "--------"
+            echo ""
+            echo "<<< --------"
+            echo "<<< Function TODO log has been executed"
+            echo "<<< --------"
             todo
             ;;
+        Delete_temp_files)
+            if git ls-files --exclude-standard --others | grep ".tmp"; then
+            echo ""
+            echo "<<< --------"
+            echo "<<< Deleting all untracked temporary files"
+            echo "<<< ----"
+            echo "<<< The following file(s) have been deleted"
+            echo ""
+            git ls-files --exclude-standard --others | grep ".tmp"
+            echo ""
+            echo "<<< --------"
+            deltempfile
+            else
+            echo ""
+            echo "<<< --------"
+            echo "<<< There are no untracked temporary files to be deleted"
+            echo "<<< --------"
+            fi
+            ;;
         Exit)
-            echo "--------"
-            echo "Exiting the script"
-            echo "--------"
+            echo ""
+            echo "<<< --------"
+            echo "<<< Exiting the script"
+            echo "<<< --------"
+            echo ""
             exit 0
             ;;
         *)
-            echo "--------"
-            echo "Error: Please try again"
-            echo "--------"
+            echo ""
+            echo "<<< --------"
+            echo "<<< Error: Please try again"
+            echo "<<< --------"
             ;;
     esac
 done
