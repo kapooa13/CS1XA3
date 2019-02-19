d30fa7b0b1019c7e1bd33aca2c2f0b23b1f25609
Ankit Kapoor <kapooa13@mcmaster.ca>
2019-02-19 00:06:40 +0000

    Implemented Compile error log feature in project_analyze.sh

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -6,6 +6,7 @@ PS3=$'\n'">>> Enter the number to call corresponding function: "
 cd ..
 
 
+
 function todo {
     rm -f todo.log
     touch todo.log
@@ -16,17 +17,58 @@ done
 }
 
 function deltempfile {
-    git ls-files --exclude-standard --others | grep ".tmp" | while read -r line
+    git ls-files --exclude-standard --others | grep -E ".tmp$" | while read -r line
     do
     rm "$line"
 done
 }
 
+function compile_errlog {
+    rm -f compile_fail.log
+    touch compile_fail.log
+
+    touch haskell_fail.hslog
+
+    touch python_fail.pylog
+    c="c"
+    #the above line is to help in deleting the pyc file created when py_compile runs
+
+    #Haskell Files
+    find . -name "*.hs" -type f -print0 | while IFS= read -d $'\0' file
+    do
+    if ghc -v "$file" |& grep -q "not linking"; then
+    echo "$file" >> haskell_fail.hslog
+    fi
+done
+    if grep -q "" haskell_fail.hslog; then
+    echo "The following haskell file(s) failed to compile:" >> compile_fail.log
+    cat haskell_fail.hslog >> compile_fail.log
+    fi
+    rm -f haskell_fail.hslog
+
+    #Python Files
+    find . -name "*.py" -type f -print0 | while IFS= read -d $'\0' file
+    do
+    if python -m py_compile "$file" |& grep -q ""; then
+    echo "$file" >> python_fail.pylog
+    else
+    rm "$file$c"
+    fi
+done
+    if grep -q "" python_fail.pylog; then
+    echo "The following python file(s) failed to compile:" >> compile_fail.log
+    cat python_fail.pylog >> compile_fail.log
+    fi
+    rm -f python_fail.pylog
+}
+
+
+
 echo "<<< --------"
 echo "<<< You are currently running project_analyze.sh"
 echo "<<< --------"
 
-select answer in TODO_log Delete_temp_files Exit
+select answer in TODO_log Delete_temp_files Compile_error_log Exit
 do
     case $answer in
         TODO_log)
@@ -37,7 +79,7 @@ do
             todo
             ;;
         Delete_temp_files)
-            if git ls-files --exclude-standard --others | grep ".tmp"; then
+            if git ls-files --exclude-standard --others | grep -E ".tmp$"; then
             echo ""
             echo "<<< --------"
             echo "<<< Deleting all untracked temporary files"
@@ -54,6 +96,13 @@ do
             echo "<<< There are no untracked temporary files to be deleted"
             echo "<<< --------"
             fi
+            ;;
+        Compile_error_log)
+            compile_errlog
+            echo ""
+            echo "<<< --------"
+            echo "<<< Compile error log has been executed"
+            echo "<<< --------"
             ;;
         Exit)
             echo ""
