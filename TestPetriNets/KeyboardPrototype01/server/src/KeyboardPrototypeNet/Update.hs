f91e31226e260ddad9e0389841c89e0980852933
Ankit K <kapooa13@mcmaster.ca>
2019-04-16 22:12:28 -0400

    finished server side code

--- TestPetriNets/KeyboardPrototype01/server/src/KeyboardPrototypeNet/Update.hs
+++ TestPetriNets/KeyboardPrototype01/server/src/KeyboardPrototypeNet/Update.hs
@@ -13,13 +13,11 @@ import qualified Static.Cmd as Cmd
 
 -- function called when new client connects (do not delete)
 clientConnect :: FromSuperPlace -> ClientID -> Keyboard -> (Keyboard, KeyboardPlayer)
-clientConnect fsp clientID keyboard =
-    error "Please fill out clientConnect function for the KeyboardPrototypeNet net."
+clientConnect fsp clientID keyboard = (keyboard, KeyboardPlayer)
 
 -- functions called when a client disconnects (do not delete)
 clientDisconnectFromKeyboard :: FromSuperPlace -> ClientID -> Keyboard -> KeyboardPlayer -> Keyboard
-clientDisconnectFromKeyboard fsp clientID keyboard keyboardPlayer =
-    error "Please fill out the clientDisconnectFromKeyboard function for the KeyboardPrototypeNet net."
+clientDisconnectFromKeyboard fsp clientID keyboard keyboardPlayer = keyboard
 
 
 -- functions for each transition
@@ -34,8 +32,8 @@ updateBoardKeyPressed :: FromSuperPlace ->
 updateBoardKeyPressed fsp clientId (BoardKeyPressed myKeyInteger) keyboard lstKeyboard =
     let
         fromKeyboard :: (ClientID, KeyboardPlayer) -> BoardKeyPressedfromKeyboard
-        fromKeyboard (pId, pkeyboard) = error "Please fill in function stub."
-
+        fromKeyboard (pId, pkeyboard) =
+                BoardKeyPressed_KeyboardtoKeyboard KeyboardPlayer (DeflatedBoardKey myKeyInteger)
 
     in
         (keyboard, fromKeyboard)
