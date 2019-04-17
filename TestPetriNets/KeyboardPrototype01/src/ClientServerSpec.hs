120d9b349f30976609b55410bfcd9f27021259bb
Ankit K <kapooa13@mcmaster.ca>
2019-04-16 20:47:01 -0400

    filled out ClientServerSpec.hs

--- TestPetriNets/KeyboardPrototype01/src/ClientServerSpec.hs
+++ TestPetriNets/KeyboardPrototype01/src/ClientServerSpec.hs
@@ -8,58 +8,79 @@ import TypeHelpers
 --directory to output the generated files
 outputDirectory = "."
 --where the generator is
-generatorRoot = "../petri-app-land"
+generatorRoot = "../../../petri-app-land"
 
-exampleNet :: Net
-exampleNet =
+{-
+- Layout for Keyboard prototype
+-
+   Key    CorrespondingInt
+
+-- white keys
+    q           0
+    w           1
+    e           2
+    r           3
+    t           4
+    y           5
+    u           6
+    i           7
+    o           8
+    p           9
+    [           10
+    ]           11
+
+-- black keys
+    2           12
+    3           13
+    4           14
+    6           15
+    7           16
+    9           17
+    0           18
+    -           19
+-}
+
+myKeyInt = dt (IntRangeT 0 19) "myKeyInteger" "representative of key"
+
+keyPrototypeNet :: Net
+keyPrototypeNet =
     let
-        placeOne =
-            Place "FirstPlace" 
+        keyboardPlace =
+            Place "Keyboard" 
                     [] --server state (persistent for this place)
                     [] --player state (client state stored on server)
                     [] --client state (state stored on client)
                     Nothing
 
-        placeTwo =
-            Place "SecondPlace"
-                    []
-                    []
-                    []
-                    Nothing
-
-        goToTwo =                 
+        boardKeyPressed =                 
             Transition
                 OriginClientOnly
-                (constructor "GoToTwo" [])
-                [("FirstPlace", Just ("SecondPlace", constructor "WentToSecondPlace" [], Nothing))
-                ,("SecondPlace", Nothing) --some people will stay
+                (constructor "BoardKeyPressed" [myKeyInt])
+                [("Keyboard", Just ("Keyboard", constructor "DeflatedBoardKey" [myKeyInt], Just "NoOp"))
                 ]
                 Nothing
 
-        goToOne =                 
-            Transition
-                OriginClientOnly
-                (constructor "GoToOne" [])
-                [("SecondPlace", Just ("FirstPlace", constructor "WentToFirstPlace" [], Nothing))
-                ,("SecondPlace", Nothing)
-                ]
+        noOp =
+            ClientTransition        -- Note that this is a client transition only
+                (msg "NoOp" [])
+                "Keyboard"          -- only from and to Keyboard
                 Nothing
+
     in
         Net
-            "MyExampleNet"  -- name of the petri net (by convention, suffixed by "Net")
-            "FirstPlace"      -- starting place when a user logs in
-            [placeOne
-            ,placeTwo
+            "KeyboardPrototypeNet"  -- name of the petri net (by convention, suffixed by "Net")
+            "Keyboard"      -- starting place when a user logs in
+            [keyboardPlace
             ]  -- list of all defined places
-            [goToTwo
-            ,goToOne
+            [noOp
+            ,boardKeyPressed
             ]   -- list of all defined transitions
             []  -- list of installed plugins
 
 -- the entire client-server app
 clientServerApp :: ClientServerApp
 clientServerApp =
-    ( "MyExampleNet"            --starting net for a client
-    , [exampleNet]              --all the nets in this client/server app (current only one is supported at a time)
+    ( "KeyboardPrototypeNet"            --starting net for a client
+    , [keyPrototypeNet]              --all the nets in this client/server app (current only one is supported at a time)
     , []       --extra client types used in states or messages
     )
