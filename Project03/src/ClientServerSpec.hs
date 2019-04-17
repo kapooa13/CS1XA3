07dc053e78be125c215756672b7c95e094c9b336
Ankit K <kapooa13@mcmaster.ca>
2019-04-17 19:32:29 -0400

    filled out specs for Project03

--- Project03/src/ClientServerSpec.hs
+++ Project03/src/ClientServerSpec.hs
@@ -8,59 +8,114 @@ import TypeHelpers
 --directory to output the generated files
 outputDirectory = "."
 --where the generator is
-generatorRoot = "../../petri-app-land/"
---              "~/petri-app-land"
+generatorRoot = "../../petri-app-land"
+
+
+-- DoctypeT variables
+
+myKeyInt = dt (IntRangeT 0 20) "myKeyInt" "representative of Key"
+
+myColor = dt (IntRangeT 0 24) "myColor" "represents index of colour to be used by client"
+
+myKeyStateBool = dt BoolT "keyStateBool" "represents state of Key" -- note: True == key being pressed
+
+myDict = DictT myKeyInt myKeyStateBool
+
+clientKeyStateDict = dt myDict "clientKeyStateDict" "dictionary representing state of keys"
+
+
+{-
+- Layout for Keyboard keys
+-
+   Key    CorrespondingInt
+
+-- white keys
+    q           0
+    w           1
+    e           2
+    r           3
+    t           4
+    y           5
+    u           6
+    i           7
+    o           8
+    p           9
+    [           10
+    ]           11
+
+-- black keys
+    2           12
+    3           13
+    4           14
+    6           15
+    7           16
+    9           17
+    0           18
+    -           19
+-}
 
 keyboardNet :: Net
 keyboardNet =
     let
-        mainMenu =
-            Place "MainMenu" 
+        keyboardPlace =
+            Place "Keyboard" 
                     [] --server state (persistent for this place)
                     [] --player state (client state stored on server)
-                    [] --client state (state stored on client)
+                    [clientKeyStateDict, myColor] --client state (state stored on client)
                     Nothing
 
-        keyboardRoom =
-            Place "KeyboardRoom"
-                    []
-                    []
-                    []
-                    Nothing
-
-        goToTwo =                 
+        boardKeyPressed =                 
             Transition
                 OriginClientOnly
-                (constructor "GoToTwo" [])
-                [("FirstPlace", Just ("SecondPlace", constructor "WentToSecondPlace" [], Nothing))
-                ,("SecondPlace", Nothing) --some people will stay
+                (constructor "BoardKeyPressed" [myKeyInt])
+                [("Keyboard", Just ("Keyboard", constructor "BoardKeyUnpressed" [myKeyInt], Just "NoOp"))
                 ]
                 Nothing
 
-        goToOne =                 
+        noOp =
+            ClientTransition        -- Note that this is a client transition only
+                (msg "NoOp" [])
+                "Keyboard"          -- from and to Keyboard
+                Nothing
+
+        makeDark =
             Transition
                 OriginClientOnly
-                (constructor "GoToOne" [])
-                [("SecondPlace", Just ("FirstPlace", constructor "WentToFirstPlace" [], Nothing))
-                ,("SecondPlace", Nothing)
+                (constructor "MakeDark" [myKeyInt])
+                [("Keyboard", Just ("Keyboard", constructor "MadeKeyDark" [myKeyInt], Nothing))
                 ]
                 Nothing
+
+        makeLight =
+            Transition
+                OriginClientOnly
+                (constructor "MakeLight" [myKeyInt])
+                [("Keyboard", Just ("Keyboard", constructor "MadeKeyLight" [myKeyInt], Nothing))
+                ]
+                Nothing
+
+        randomColor =
+            Transition
+                OriginClientOnly
+                (constructor "RandomColorNumber" [myColor])
+
     in
         Net
-            "MyExampleNet"  -- name of the petri net (by convention, suffixed by "Net")
-            "FirstPlace"      -- starting place when a user logs in
-            [placeOne
-            ,placeTwo
-            ]  -- list of all defined places
-            [goToTwo
-            ,goToOne
-            ]   -- list of all defined transitions
-            []  -- list of installed plugins
+            "MyKeyboardNet"     -- name of the petri net (by convention, suffixed by "Net")
+            "Keyboard"          -- starting place when a user logs in
+            [keyboardPlace
+            ]                   -- list of all defined places
+            [boardKeyPressed
+            ,noOp
+            ,makeDark
+            ,makeLight
+            ]                   -- list of all defined transitions
+            []                  -- list of installed plugins
 
 -- the entire client-server app
 clientServerApp :: ClientServerApp
 clientServerApp =
-    ( "MyExampleNet"            --starting net for a client
-    , [exampleNet]              --all the nets in this client/server app (current only one is supported at a time)
-    , []       --extra client types used in states or messages
+    ( "KeyboardNet"              --starting net for a client
+    , [keyboardNet]              --all the nets in this client/server app (current only one is supported at a time)
+    , []                         --extra client types used in states or messages
     )
